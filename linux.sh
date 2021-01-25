301d5726203cc6ff3f4411eee90e817c1134379f
Hugo <40466699+hugo-paredes@users.noreply.github.com>
2021-01-25 14:01:36 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,22 +1,5 @@
 #!/bin/bash -x
 
-#
-# Script that installs `terraform` in a Linux instance. It expects that `unzip` and `wget` tools are already installed.
-# Specify the version by passing the terraform as a parameter or by setting the environment variable TF_VERSION
-#
-# How to use?
-#
-#  1) Download the bash script to a local folder.
-#  2) Set the environment variable TF_VERSION to the desired version
-#     or pass the version as a parameter to the script.
-#     $ export TF_VERSION=0.12.29
-#     $ ./install_terraform.sh
-#     OR
-#     $ ./install_terraform.sh 0.12.29
-#  OR
-#  1) 
-# 
-
 TF_VERSION="${1:-`echo ${TF_VERSION}`}"
 
 if [ -z "${TF_VERSION}" ]; then
